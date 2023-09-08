35569267d4e80bc8a65f61599992823333bb79af
Sankrita <patelsankrita6960@gmail.com>
2023-09-08 22:36:50 +0530

    Data ingestion

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import sys
 from src.ml_project.logger import logging
-from src.ml_project.exception import CustomEceptions
+from src.ml_project.exception import CustomExceptions
+from src.ml_project.components.data_ingestion import DataIngestion
+from src.ml_project.components.data_ingestion import DataIngestionConfig
+
 
 if __name__ == '__main__':
     logging.info("The executaion has been created")
 
     try:
-        a = 1/0
+        #data_ingestion = DataIngestionConfig()
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custom Exception")
-        raise CustomEceptions(e, sys)
+        raise CustomExceptions(e, sys)
         
--- src/ml_project/exception.py
+++ src/ml_project/exception.py
@@ -9,7 +9,7 @@ def error_message_detail(error, error_detail:sys):
     file_name,exc_tb.tb_lineno, str(error))
     return error_message
 
-class CustomEceptions(Exception):
+class CustomExceptions(Exception):
     def __init__(self, error_message, error_details:sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_details)
